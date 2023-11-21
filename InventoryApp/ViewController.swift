de966e1a76b8aea4270854b9372a7b21c02166c7
immisius <immisius@gmail.com>
2023-11-21 20:53:44 +0900

    軽微な調整

--- InventoryApp/ViewController.swift
+++ InventoryApp/ViewController.swift
@@ -1,60 +1,104 @@
-//
-//  ViewController.swift
-//  InventoryApp
-//
-//  Created by 遠藤洸亮 on R 5/11/21.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    // 在庫データのテキストフィールド
     @IBOutlet weak var textFieldDataBoxes: UITextField!
     @IBOutlet weak var textFieldDataUnits: UITextField!
+    @IBOutlet weak var textFieldUnitsPerBox: UITextField!
+    // 実際の在庫のテキストフィールド
     @IBOutlet weak var textFieldActualBoxes: UITextField!
     @IBOutlet weak var textFieldActualUnits: UITextField!
+
+    // 結果表示用のラベル
     @IBOutlet weak var resultLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // テキストフィールドにイベントリスナーを設定
+
+        // 入力変更時に在庫計算を実行するようリスナーを設定
         textFieldDataBoxes.addTarget(self, action: #selector(calculateInventory), for: .editingChanged)
         textFieldDataUnits.addTarget(self, action: #selector(calculateInventory), for: .editingChanged)
+        textFieldUnitsPerBox.addTarget(self, action: #selector(calculateInventory), for: .editingChanged)
         textFieldActualBoxes.addTarget(self, action: #selector(calculateInventory), for: .editingChanged)
         textFieldActualUnits.addTarget(self, action: #selector(calculateInventory), for: .editingChanged)
     }
 
+    // 実際の箱数を増減させるアクション
+    @IBAction func adjustActualBoxes(_ sender: UIButton) {
+        let adjustment = sender.tag // ボタンのタグを使用して調整値を取得
+        adjustTextFieldValue(textField: textFieldActualBoxes, adjustment: sender.tag)
+        calculateInventory()
+    }
+
+    // 実際の本数を増減させるアクション
+    @IBAction func adjustActualUnits(_ sender: UIButton) {
+        let adjustment = sender.tag
+        adjustTextFieldValue(textField: textFieldActualUnits, adjustment: sender.tag)
+        calculateInventory()
+    }
+    
+    // データの箱数を増減させるアクション
+    @IBAction func adjustDataBoxes(_ sender: UIButton) {
+        let adjustment = sender.tag // ボタンのタグを使用して調整値を取得
+        adjustTextFieldValue(textField: textFieldDataBoxes, adjustment: sender.tag)
+        calculateInventory()
+    }
+
+    // データの本数を増減させるアクション
+    @IBAction func adjustDataUnits(_ sender: UIButton) {
+        let adjustment = sender.tag
+        adjustTextFieldValue(textField: textFieldDataUnits, adjustment: sender.tag)
+        calculateInventory()
+    }
+    
+    @IBAction func adjustUnitsPerBox(_ sender: UIButton) {
+        let adjustment = sender.tag
+        adjustTextFieldValue(textField: textFieldUnitsPerBox, adjustment: sender.tag)
+        calculateInventory()
+    }
+    
+    // テキストフィールドの値を増減させる汎用メソッド
+    func adjustTextFieldValue(textField: UITextField, adjustment: Int) {
+        let currentValue = Int(textField.text ?? "") ?? 0 // 空の場合は0とする
+        let newValue = currentValue + adjustment
+        if newValue >= 0 {
+            textField.text = "\(newValue)"
+        }
+    }
+
+
+    
+    
+    // 在庫計算を実行するメソッド
     @objc func calculateInventory() {
-        // テキストフィールドから値を取得
         let dataBoxes = Int(textFieldDataBoxes.text ?? "") ?? 0
         let dataUnits = Int(textFieldDataUnits.text ?? "") ?? 0
+        let unitsPerBox = Int(textFieldUnitsPerBox.text ?? "") ?? 0
         let actualBoxes = Int(textFieldActualBoxes.text ?? "") ?? 0
         let actualUnits = Int(textFieldActualUnits.text ?? "") ?? 0
 
-        // 在庫の合計を計算
-        let totalDataUnits = (dataBoxes * dataUnits) + dataUnits
-        let totalActualUnits = (actualBoxes * actualUnits) + actualUnits
+        let totalDataUnits = dataBoxes * unitsPerBox + dataUnits
+        let totalActualUnits = actualBoxes * unitsPerBox + actualUnits
 
-        // 差を計算
         let difference = totalDataUnits - totalActualUnits
 
-        // 結果を表示
         if difference > 0 {
-            // 欠品の場合
-            let missingBoxes = difference / dataUnits
-            let missingUnits = difference % dataUnits
-            resultLabel.text = "欠品: \(missingBoxes) 箱と \(missingUnits) 本"
+            if unitsPerBox != 0 { // 0での除算を防ぐ
+                let missingBoxes = difference / unitsPerBox
+                let missingUnits = difference % unitsPerBox
+                resultLabel.text = "欠品: \(missingBoxes) 箱と \(missingUnits) 本"
+                resultLabel.textColor = UIColor.systemTeal // 水色に設定
+            }
         } else if difference < 0 {
-            // 余剰の場合
-            let extraBoxes = abs(difference) / dataUnits
-            let extraUnits = abs(difference) % dataUnits
-            resultLabel.text = "余剰: \(extraBoxes) 箱と \(extraUnits) 本"
+            if unitsPerBox != 0 {
+                let extraBoxes = abs(difference) / unitsPerBox
+                let extraUnits = abs(difference) % unitsPerBox
+                resultLabel.text = "余剰: \(extraBoxes) 箱と \(extraUnits) 本"
+                resultLabel.textColor = UIColor.systemOrange // オレンジ色に設定
+            }
         } else {
-            // 在庫が一致する場合
             resultLabel.text = "在庫は一致しています"
+            resultLabel.textColor = UIColor.systemGreen // またはデフォルトの色に設定
         }
     }
 }
-
-
