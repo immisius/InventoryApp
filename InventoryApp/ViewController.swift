42543e46e9fa404737305cb36bc194c0734dc2e5
immisius <immisius@gmail.com>
2023-11-21 16:45:15 +0900

    Initial Commit

---
+++ InventoryApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InventoryApp
+//
+//  Created by 遠藤洸亮 on R 5/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
